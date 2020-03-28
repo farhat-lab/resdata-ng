948be590673eeaa18ba08b88f8866374a30844e8
Luca Freschi <luca_freschi@hms.harvard.edu>
2020-03-27 23:18:05 -0400

    Added new function ("convert_to_biosample_simplified") to convert NCBI IDs to biosamples / useful for debug with weird IDs

--- bin/convert_to_biosample.py
+++ bin/convert_to_biosample.py
@@ -38,7 +38,8 @@ def parse_response(ncbi_id, response) -> str:
 	return(biosample)
 
 def convert_to_biosample(list_ncbi_ids: list) -> list:
-	with IncrementalBar('Converting...', max=len(list_ncbi_ids), suffix='%(percent).1f%%') as bar:
+	ncbi_ids_with_issues=[]
+	with IncrementalBar("Converting...".format(value), max=len(list_ncbi_ids), suffix='%(percent).1f%%') as bar:
 		list_of_biosamples=[]
 		for ncbi_id in list_ncbi_ids:
 			if ncbi_id == "":
@@ -49,12 +50,50 @@ def convert_to_biosample(list_ncbi_ids: list) -> list:
 					list_of_biosamples.append(ncbi_id)
 				else:
 					request = "http://trace.ncbi.nlm.nih.gov/Traces/sra/sra.cgi?save=efetch&db={}&rettype=runinfo&term={}".format(db, ncbi_id)
-					response = requests.get(url=request,stream=True)
+					response = requests.get(url=request,stream=True,timeout=60)
 					response.encoding = 'utf-8'
-					biosample=parse_response(ncbi_id, response.text)
+					try:
+						biosample=parse_response(ncbi_id, response.text)
+					except:
+						biosample=""
+						ncbi_ids_with_issues.append(ncbi_id)
 					list_of_biosamples.append(biosample)
 			bar.next()
+		print("* I found {} issues in converting the IDs you provided".format(len(ncbi_ids_with_issues)))
+		if len(ncbi_ids_with_issues)!=0:
+			print("* Here are the problematic IDs:")
+			print(",".join(ncbi_ids_with_issues))
 		return(list_of_biosamples)
+
+def convert_to_biosample_simplified(list_ncbi_ids: list) -> list:
+	ncbi_ids_with_issues=[]
+	list_of_biosamples=[]
+	counter=0
+	for ncbi_id in list_ncbi_ids:
+		counter = counter + 1
+		print("* analysing {} ({} of {})".format(ncbi_id, counter, len(list_ncbi_ids)))
+		if ncbi_id == "":
+			list_of_biosamples.append("")
+		else:
+			db=get_ncbi_id_type(ncbi_id)
+			if db == "biosample":
+				list_of_biosamples.append(ncbi_id)
+			else:
+				request = "http://trace.ncbi.nlm.nih.gov/Traces/sra/sra.cgi?save=efetch&db={}&rettype=runinfo&term={}".format(db, ncbi_id)
+				response = requests.get(url=request,stream=True,timeout=60)
+				response.encoding = 'utf-8'
+				try:
+					biosample=parse_response(ncbi_id, response.text)
+				except:
+					biosample=""
+					ncbi_ids_with_issues.append(ncbi_id)
+				list_of_biosamples.append(biosample)
+	print("* I found {} issues in converting the IDs you provided".format(len(ncbi_ids_with_issues)))
+	if len(ncbi_ids_with_issues)!=0:
+		print("* Here are the problematic IDs:")
+		print(",".join(ncbi_ids_with_issues))
+	return(list_of_biosamples)
+
 
 def write_tsv(list_ncbi_ids, list_of_biosamples, file_out):
 	with open(file_out,"w") as outf:
