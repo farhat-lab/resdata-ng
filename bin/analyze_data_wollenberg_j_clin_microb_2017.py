e648f3bafa033bd4e4c1baa4d2990234d5ee9c62
Luca Freschi <luca_freschi@hms.harvard.edu>
2020-03-27 23:13:59 -0400

    Added new function to generate_amr_summary_table.py. Writes down a file with the collisions (collisions_summary.txt)

---
+++ bin/analyze_data_wollenberg_j_clin_microb_2017.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+import pandas as pd
+from progress.bar import IncrementalBar
+
+# load the data frame
+df = pd.read_csv("resistance_data/sources/wollenberg_j_clin_microb_2017/wollenberg_2017_curated_phenotypes.tsv",sep="\t")
+
+# I standardize the names of the antibiotics
+cols = df.columns.tolist()
+cols[2:] = ["ETHAMBUTOL","ISONIAZID","RIFAMPICIN","RIFABUTIN","PYRAZINAMIDE",
+"AMIKACIN","KANAMYCIN","CAPREOMYCIN","CYCLOSERINE","ETHIONAMIDE","OXIFLOXACIN",
+"PARA_AMINOSALICYLIC_ACID","STREPTOMYCIN"]
+df.columns = cols
+
+# No need to convert the biosamples
+
+list_of_antibiotics = cols[2:]
+
+with open("resistance_data/summary_tables/wollenberg_j_clin_microb_2017.res", "w") as outf:
+    for idx, row in df.iterrows(): 
+        for antb in list_of_antibiotics:
+            res_status = row[antb]
+            if pd.isnull(res_status):
+                continue
+            else:
+                outf.write("\t".join([row[0], antb, res_status, "WOLLENBERG_J_CLIN_MICROB_2017"]) + "\n")
+
+
--- bin/generate_amr_summary_table.py
+++ bin/generate_amr_summary_table.py
@@ -103,7 +103,47 @@ def generate_summary_from_res_remove_collisions(res_files: list,out_file: str) -
     print("[INFO] I generated the report! ({})".format(out_file))
     return()
 
+def generate_summary_from_res_remove_collisions_write_log(res_files: list,out_file: str, out_file_collisions: str) -> None:
+    """Takes a list of res files as input and builds a summary table for the resistance data (out_file). It checks for collisions and removes them"""
+    #the format of a res file is: <biosample> <antb> <res_class> <tags>
+    d_res_class=recursive_defaultdict()
+    d_antb={}
+    for current_file in res_files:
+        inp = csv.reader(open(current_file,"r"), delimiter='\t')
+        for entry in inp:
+            biosample=entry[0]
+            antb=entry[1]
+            d_antb[antb]=1
+            res_class=entry[2]
+            tags=entry[3]
+            d_res_class[biosample][antb]=res_class
+    collisions=get_collisions(res_files)
+    for collision in collisions:
+        explode_collision=collision.split("@")
+        # I set the entry to ""
+        d_res_class[explode_collision[0]][explode_collision[1]]=""
+    with open(out_file, "w") as outf:
+        outf.write("Isolate\t{}\n".format("\t".join(sorted(d_antb.keys()))))
+        for biosample in sorted(d_res_class.keys()):
+            entry=[biosample]
+            for antb in sorted(d_antb.keys()):
+                try:
+                    res_class=d_res_class[biosample][antb]
+                    if res_class in ["R","S"]:
+                        entry.append(res_class)
+                    else:
+                        entry.append("")
+                except:
+                    entry.append("")
+                    pass
+            outf.write("\t".join(entry)+"\n")
+    print("[INFO] I generated the report! ({})".format(out_file))
+    with open(out_file_collisions, "w") as outf:
+        for collision in collisions:
+            outf.write(collision+"\n")
+    print("[INFO] I generated the report of the collisions! ({})".format(out_file_collisions))
+    return()
 
 list_res_files=glob.glob("resistance_data/summary_tables/*.res")
-generate_summary_from_res_remove_collisions(list_res_files , "./resistance_data/summary_tables/resistance_summary.txt")
+generate_summary_from_res_remove_collisions_write_log(list_res_files , "./resistance_data/summary_tables/resistance_summary.txt", "./resistance_data/summary_tables/collisions_summary.txt")
 
